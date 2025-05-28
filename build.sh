cb5b1dd09f609dcfcf63dce338e931484f88295d
akshaykarthicks <akshayyourbest153@gmail.com>
2025-05-28 14:02:41 +0530

    Update build.sh with clearer comments and add WEB_CONCURRENCY to render.yaml

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Make sure we're in the directory with manage.py
 cd weathers
+
+# Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
