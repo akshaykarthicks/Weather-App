b890299c9ebb2267d1753bf62c6498c8c96f38e4
akshaykarthicks <akshayyourbest153@gmail.com>
2025-05-28 14:42:32 +0530

    Update WSGI settings and Gunicorn command in render.yaml

--- weathers/weathers/wsgi.py
+++ weathers/weathers/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weathers.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weathers.weathers.settings')
 
 application = get_wsgi_application()
