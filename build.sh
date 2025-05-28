661d9af2cbbe9ff195ea1d0d26f798ff98a8caa4
akshaykarthicks <akshayyourbest153@gmail.com>
2025-05-28 13:58:56 +0530

    Fix build configuration to point to correct manage.py location

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python weathers/manage.py collectstatic --no-input
+python weathers/manage.py migrate
